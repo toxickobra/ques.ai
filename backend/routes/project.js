7f08012d6c529a66c9b2157863e9d91fbc33bc10
toxickobra <pprashantsinghchauhan11@gmail.com>
2024-08-18 18:05:09 +0530

    final commit

--- backend/routes/project.js
+++ backend/routes/project.js
@@ -21,4 +21,6 @@ router.get('/', auth, getAllProjectsForUser);
 // Delete a project by ID
 router.delete('/:id', auth, deleteProject);
 
+route.delete(':/id',auth , deleteProject);
+
 module.exports = router;
