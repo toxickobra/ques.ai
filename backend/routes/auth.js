4e6d8339e3f79ca66ec12ff465c21b9e8f457bd6
Prashant Singh Chauhan <110364790+toxickobra@users.noreply.github.com>
2024-09-11 16:48:04 +0530

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 // routes/authRoutes.js
 
 const express = require('express');
-const { registerUser, loginUser, getProfile } = require('../controllers/authController');
+const { registerUser, loginUser, getProfile, updateUsername } = require('../controllers/authController');
 const authenticateToken = require('../middleware/auth');
 const router = express.Router();
 
@@ -16,7 +16,10 @@ router.get('/verify', authenticateToken, (req, res) => {
   res.sendStatus(200); // If the token is valid, send back a 200 status
 });
 
-// Profile route (this is the route that was missing)
+// Profile route
 router.get('/profile', authenticateToken, getProfile);
+
+// Update username route
+router.put('/username', authenticateToken, updateUsername); // Corrected line
 
 module.exports = router;
